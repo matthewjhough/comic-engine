bd7ce35f2a6ac4d7c7f080c72d03a0707ca2b338
Matthew Hough <matthough1@icloud.com>
2019-12-07 14:57:40 -0500

    add graphql to project

---
+++ ComicEngine.Client/Graphql/Query.cs
@@ -0,0 +1,5 @@
+namespace ComicEngine.Client.Graphql {
+    public class Query {
+        public string Hello => "World";
+    }
+}
--- ComicEngine.Client/Startup.cs
+++ ComicEngine.Client/Startup.cs
@@ -1,93 +1,93 @@
+using ComicEngine.Client.Data;
+using ComicEngine.Client.Graphql;
+using ComicEngine.Client.Models;
+using HotChocolate;
+using HotChocolate.AspNetCore;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
-using ComicEngine.Client.Data;
-using ComicEngine.Client.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ComicEngine.Client
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace ComicEngine.Client {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<ApplicationDbContext> (options =>
+                options.UseSqlite (
+                    Configuration.GetConnectionString ("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDefaultIdentity<ApplicationUser> ()
+                .AddEntityFrameworkStores<ApplicationDbContext> ();
 
-            services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+            services.AddIdentityServer ()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext> ();
 
-            services.AddAuthentication()
-                .AddIdentityServerJwt();
+            services.AddAuthentication ()
+                .AddIdentityServerJwt ();
 
-            services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddControllersWithViews ();
+            services.AddRazorPages ();
 
             // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
+            services.AddSpaStaticFiles (configuration => {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddGraphQL (services =>
+                SchemaBuilder.New ()
+                .AddServices (services)
+                .AddQueryType<Query> ()
+                .Create ());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+                app.UseDatabaseErrorPage ();
+            } else {
+                app.UseExceptionHandler ("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts ();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
+            app.UseHttpsRedirection ();
+            app.UseStaticFiles ();
+            app.UseSpaStaticFiles ();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthentication();
-            app.UseIdentityServer();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
+            app.UseAuthentication ();
+            app.UseIdentityServer ();
+            app.UseAuthorization ();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllerRoute (
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
+                endpoints.MapRazorPages ();
             });
 
-            app.UseSpa(spa =>
-            {
+            app.UseGraphQL ();
+            app.UsePlayground ();
+
+            app.UseSpa (spa => {
                 spa.Options.SourcePath = "ClientApp";
 
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
+                if (env.IsDevelopment ()) {
+                    spa.UseReactDevelopmentServer (npmScript: "start");
                 }
             });
         }
