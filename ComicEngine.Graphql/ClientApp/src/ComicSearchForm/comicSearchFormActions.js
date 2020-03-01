e8fccef95c39756eda1e870b5868eb57a7f93b9f
Matthew Hough <matthough1@icloud.com>
2020-02-29 22:06:18 -0500

    remove unused code

--- ComicEngine.Graphql/ClientApp/src/ComicSearchForm/comicSearchFormActions.js
+++ ComicEngine.Graphql/ClientApp/src/ComicSearchForm/comicSearchFormActions.js
@@ -2,10 +2,6 @@ import { UPDATE_TITLE_INPUT, UPDATE_ISSUE_NUMBER_INPUT } from '../actionTypes';
 import { fetchComicFromTitleAndIssueNumber } from '../graphqlClient/graphqlClient';
 import { setResults, toggleLoading } from '../ComicResults/comicResultsActions';
 
-/**
- *
- * @param {string} title
- */
 export function updateTitleInput(title) {
   return {
     type: UPDATE_TITLE_INPUT,
@@ -13,10 +9,6 @@ export function updateTitleInput(title) {
   };
 }
 
-/**
- *
- * @param {string} issueNumber
- */
 export function updateIssueNumberInput(issueNumber) {
   return {
     type: UPDATE_ISSUE_NUMBER_INPUT,
@@ -24,10 +16,6 @@ export function updateIssueNumberInput(issueNumber) {
   };
 }
 
-/**
- *
- * @param {object} comicForm
- */
 export function updateResultsFromForm(dispatch) {
   return function(comicForm) {
     return fetchComicFromTitleAndIssueNumber(comicForm)
