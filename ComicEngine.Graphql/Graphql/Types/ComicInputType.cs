91b2defe60ef543f8ac4de07784614e81bc1ab86
Matthew Hough <matthough1@icloud.com>
2020-03-15 13:54:19 -0400

    abstract out comic fields to base class

--- ComicEngine.Graphql/Graphql/Types/ComicInputType.cs
+++ ComicEngine.Graphql/Graphql/Types/ComicInputType.cs
@@ -1,15 +1,6 @@
+using System.Collections.Generic;
+using ComicEngine.Common.Comic;
+
 namespace ComicEngine.Graphql.Graphql.Types {
-    public class ComicInputType {
-        public int Id { get; set; }
-
-        public string Copyright { get; set; }
-
-        public double IssueNumber { get; set; }
-
-        public string Title { get; set; }
-
-        public string Upc { get; set; }
-
-        public string Description { get; set; }
-    }
+    public class ComicInputType : ComicBase { }
 }
